861f5732d5a3065b466815a6aab5ed17a5195ca3
Sandru Gabriel <gabi200611@yahoo.com>
2023-04-19 17:15:16 +0300

    done

---
+++ AplicatieFreeBook/Models/Imprumut.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieFreeBook.Models
+{
+    internal class Imprumut
+    {
+
+        private int id;
+        private int id_carte;
+        private string email;
+        private DateTime data_Imprumut;
+
+        public Imprumut(int id, int id_carte, string email, DateTime data_Imprumut)
+        {
+            this.id = id;
+            this.id_carte = id_carte;
+            this.email = email;
+            this.data_Imprumut = data_Imprumut;
+        }
+
+        public Imprumut(string text)
+        {
+
+            string[] prop = text.Split('*');
+
+            this.id = int.Parse(prop[0]);
+            this.id_carte = int.Parse(prop[1]);
+            this.email = prop[2];
+            this.data_Imprumut = DateTime.Parse(prop[3]);
+
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public int getId_carte()
+        {
+            return id_carte;
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        public DateTime getData_Imprumut()
+        {
+            return data_Imprumut;
+        }
+
+
+
+    }
+}
